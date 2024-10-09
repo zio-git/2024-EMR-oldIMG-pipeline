2470f4bd8a9f81a61999bb4017dd65031274b4fd
Your Name <you@example.com>
2024-10-09 06:09:05 +0000

    adding apk script

--- edit.sh
+++ edit.sh
@@ -5,6 +5,8 @@ sed -i "34d" api_shippingx.py
 sed -i "33 acluster = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_cluster/$cluster')" api_shippingx.py
 sed -i "34d" core_shippingx.py
 sed -i "33 acluster = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_cluster/$cluster')" core_shippingx.py
+sed -i "34d" coreAPK_shippingx.py
+sed -i "33 acluster = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_cluster/$cluster')" coreAPK_shippingx.py
 
 # clear files
 truncate -s 0 updated_sites.txt
